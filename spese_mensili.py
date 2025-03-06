174531caaba71a082c313c49412f2a254b6b55d7
Nenes92 <longheu.emanuele@gmail.com>
2025-03-06 21:20:57 +0100

    spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -1010,7 +1010,8 @@ def crea_grafico_stipendi(data):
         y=alt.Y("Valore:Q", title="Valore (€)")
     )
     line_chart = base_line.mark_line(strokeWidth=2, strokeDash=[5,5]).encode(
-        alt.Color("Categoria:N", scale=alt.Scale(domain=line_categories, range=line_color_range, title="Stipendi"))
+        alt.Color("Categoria:N", scale=alt.Scale(domain=line_categories, range=line_color_range))
+        color=alt.Color(scale=alt.Scale(title="Risparmi"))
     )
     points_chart = base_line.mark_point(shape="diamond", size=100, filled=True, opacity=0.7).encode(
         alt.Color("Categoria:N", scale=alt.Scale(domain=line_categories, range=line_color_range))
