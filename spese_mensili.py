bb7894ce7b9aa9060fb3b9c1ba227b8e75458334
Nenes92 <longheu.emanuele@gmail.com>
2025-11-08 06:43:35 +0100

    spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -59,7 +59,7 @@ SPESE = {
         "Cometa": 30,
         "Alleanza - PAC": 100,
         "Macchina": 180,
-        "Trasporti": 150,
+        "Trasporti": 200,
         "Sport": 90,
         "Psicologo": 100,
         "World Food Programme": 30,
@@ -1561,6 +1561,7 @@ with col_bol_chart:
     st.markdown(f"**Media mensile bollette:** <span style='color:#FFA500;'>{media_annua:,.2f} €</span>", unsafe_allow_html=True)
 
 st.markdown('<hr style="width: 100%; height:5px;border-width:0;color:gray;background-color:gray">', unsafe_allow_html=True)
+
 
 
 
