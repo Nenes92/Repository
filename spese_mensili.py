cef9c1b7033b4e9a12d2c92c82b8b221cedd1559
Nenes92 <longheu.emanuele@gmail.com>
2025-11-08 07:23:34 +0100

    spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -65,7 +65,7 @@ SPESE = {
         "World Food Programme": 30,
         "Beneficienza": 15,
         "Netflix": 8.5,
-        "Spotify": 4,
+        "Spotify": 3.5,
         "Disney+": 4,
         "Wind": 10,
         "BNL C.C.": 9.4,
@@ -1562,6 +1562,7 @@ with col_bol_chart:
     st.markdown(f"**Media mensile bollette:** <span style='color:#FFA500;'>{media_annua:,.2f} €</span>", unsafe_allow_html=True)
 
 st.markdown('<hr style="width: 100%; height:5px;border-width:0;color:gray;background-color:gray">', unsafe_allow_html=True)
+
 
 
 
