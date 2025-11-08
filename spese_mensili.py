d947d8a251ac9950760c6926cb294f78b1f175ff
Nenes92 <longheu.emanuele@gmail.com>
2025-11-08 07:06:17 +0100

    spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -831,7 +831,7 @@ def main():
                 totale_carta = revolut_expenses  # Usa il valore modificato per Revolut
                 colore = "#89CFF0"  # Azzurro
                 testo = "trasferire"
-                somma_valori = risparmi_mese_precedente + ({SPESE["Fisse"]["Disney+"]*3 + SPESE["Fisse"]["Spotify"]*5}) + totale_carta
+                somma_valori = risparmi_mese_precedente + {SPESE["Fisse"]["Disney+"]*3 + SPESE["Fisse"]["Spotify"]*5} + totale_carta
                 st.markdown(f'Totale da &nbsp; **<em style="color: #A0A0A0;">{testo}</em> &nbsp; su <span style="color:{colore}; text-decoration: underline;">{carta}</span>:** <span style="color:{colore}">€{totale_carta:.2f}</span> <span style="font-size: 14px; color: gray;"> &nbsp;&nbsp;( + <span style="color:{colore}; font-size: 14px;">{risparmi_mese_precedente:.2f}</span> dai Risparmi + <span style="color:{colore}; font-size: 14px;">€{SPESE["Fisse"]["Disney+"]*3 + SPESE["Fisse"]["Spotify"]*5:.2f}</span> da Disn/Spoti -> Vedrai: <span style="color:{colore}; font-size: 14px;">€{somma_valori:.2f}</span> )</span>', unsafe_allow_html=True)
             else:
                 totale_carta = sum(spese_carta.values())
@@ -1561,6 +1561,7 @@ with col_bol_chart:
     st.markdown(f"**Media mensile bollette:** <span style='color:#FFA500;'>{media_annua:,.2f} €</span>", unsafe_allow_html=True)
 
 st.markdown('<hr style="width: 100%; height:5px;border-width:0;color:gray;background-color:gray">', unsafe_allow_html=True)
+
 
 
 
