379114770e84ebdc2df3a064e1d5b6d0e7edc81e
Nenes92 <longheu.emanuele@gmail.com>
2025-11-24 08:46:20 +0100

    spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -85,7 +85,7 @@ SPESE = {
 # Dizionario delle altre entrate
 ALTRE_ENTRATE = {
     "Macchina (Mamma)": 100,
-    "Affitto Garage": 0, #/0,
+    "Seconda Entrata": 0, #/0,
     "Altro": 0
 }
 
@@ -1562,6 +1562,7 @@ with col_bol_chart:
     st.markdown(f"**Media mensile bollette:** <span style='color:#FFA500;'>{media_annua:,.2f} €</span>", unsafe_allow_html=True)
 
 st.markdown('<hr style="width: 100%; height:5px;border-width:0;color:gray;background-color:gray">', unsafe_allow_html=True)
+
 
 
 
