0f57245dbec912b65d0315eeacbf9c881a25aa2e
Nenes92 <longheu.emanuele@gmail.com>
2026-02-27 13:09:25 +0100

    spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -52,7 +52,7 @@ SPESE = {
     "Fisse": {
         "Mutuo": 0, #/431.16,
         "Bollette": 165,
-        "Condominio": 40,
+        "Condominio": 44,
         "Garages": 0, #/30,
         "Cucina": 0, #/200,
         "Pulizia Casa": 0, #/30,
@@ -1568,6 +1568,7 @@ with col_bol_chart:
     st.markdown(f"**Media mensile bollette:** <span style='color:#FFA500;'>{media_annua:,.2f} €</span>", unsafe_allow_html=True)
 
 st.markdown('<hr style="width: 100%; height:5px;border-width:0;color:gray;background-color:gray">', unsafe_allow_html=True)
+
 
 
 
