f074e5d16a082467ecaac2a498c1cda7d18ff2df
Nenes92 <longheu.emanuele@gmail.com>
2026-04-01 00:01:57 +0200

    ruba_hassan.py

--- ruba_hassan.py
+++ ruba_hassan.py
@@ -1385,7 +1385,7 @@ def crea_confronto_anno_su_anno_bollette(data):
 # SEZIONE: Storico Stipendi e Risparmi
 #######################################
 
-st.markdown('<div class="section-pill">📈 Storico</div>', unsafe_allow_html=True)
+st.markdown('<div class="section-pill">📈 Storico Stipendi</div>', unsafe_allow_html=True)
 st.title("Storico Stipendi e Risparmi")
 
 STIPENDI_HEADERS = ["Mese", "Stipendio", "Risparmi", "Messi da parte Totali"]
@@ -1658,7 +1658,7 @@ st.markdown('<hr style="width: 100%; height:1px;border-width:0;background:linear
 # SEZIONE: Storico Bollette
 #############################
 
-st.markdown('<div class="section-pill">🧾 Bollette</div>', unsafe_allow_html=True)
+st.markdown('<div class="section-pill">🧾 Storico Bollette</div>', unsafe_allow_html=True)
 st.markdown('<h1 style="font-size:2rem;font-weight:600;background:linear-gradient(90deg,#60a5fa,#a78bfa,#34d399);-webkit-background-clip:text;-webkit-text-fill-color:transparent;background-clip:text;">Storico Bollette</h1>', unsafe_allow_html=True)
 
 BOLLETTE_HEADERS = ["Mese", "Elettricità", "Gas", "Acqua", "Internet", "Tari"]
