c14dda67399430d56d150c016661b1b71c341e55
Nenes92 <longheu.emanuele@gmail.com>
2026-04-03 15:58:26 +0200

    Update spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -1015,8 +1015,8 @@ def main():
                     ),
                     legend=alt.Legend(
                         title=None,
-                        orient='right',
-                        direction='vertical',
+                        orient='bottom',
+                        direction='horizontal',
                         labelColor='rgba(255,255,255,0.65)',
                         labelFontSize=11,
                         symbolSize=40,      # più piccolo e compatto
