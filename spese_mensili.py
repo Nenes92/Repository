6471408c6cce3ba41f9cd22699f5569b86e7135c
Nenes92 <longheu.emanuele@gmail.com>
2026-04-04 01:47:19 +0200

    Update spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -876,7 +876,7 @@ def main():
 
 
         st.markdown("---")
-        col_spese_variabili_1, col_spese_variabili_2 = st.columns([1, 1.7])
+        col_spese_variabili_1, col_spese_variabili_2 = st.columns([1, 2])
         with col_spese_variabili_1:
             _sv = f"€{spese_variabili_totali:.2f}"
             st.markdown(f"""
