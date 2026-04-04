381da2e9246a316aaedbef36303a000e35a2aae2
Nenes92 <longheu.emanuele@gmail.com>
2026-04-04 21:08:06 +0200

    Update spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -1172,7 +1172,7 @@ def main():
         # Visualizzazione grafici
         with st.container():
             with st.container():
-                col_vuoto_a, col1_1, col1_2, col_vuoto_b= st.columns([0.5, 1, 1, 0.5])
+                col_vuoto_a, col1_1, col1_2, col_vuoto_b= st.columns([0.25, 1, 1, 0.1])
                 with col1_1:
                     st.altair_chart(chart_fisse, use_container_width=True)
                 with col1_2:
