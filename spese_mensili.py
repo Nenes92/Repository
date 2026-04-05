a75be355dbaac76c3ea464a5b7a00ff22edcf7a7
Nenes92 <longheu.emanuele@gmail.com>
2026-04-05 22:23:33 +0200

    Update spese_mensili.py

--- spese_mensili.py
+++ spese_mensili.py
@@ -1185,6 +1185,7 @@ def main():
         # Visualizzazione grafici
         with st.container():
             with st.container():
+                st.markdown('<div class="section-pill">🏠 Spese Fisse</div>', unsafe_allow_html=True)
                 col_vuoto_a, col1_1, col1_2, col_vuoto_b= st.columns([0.07, 0.5, 1, 0.1])
                 with col1_1:
                     st.altair_chart(chart_fisse, use_container_width=True)
